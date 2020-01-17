243b258794efeceb72fadb453731a0f5d092ff23
Solito Reyes II <solitonyc@gmail.com>
2020-01-17 18:24:26 -0500

    added all section pages with styles

--- src/sections/Main.js
+++ src/sections/Main.js
@@ -12,7 +12,7 @@ export default class Main extends Component {
                 <div className="main_page_section" id="main">
                 <div style={style} className='main_section_background'>
                     <div className="main_content_container">  
-                        <div className="main_headline">Reach Higher</div>
+                        <div className="main_headline">Main Page Headline</div>
                         <div className="main_paragraph_01">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
--- src/sections/Page5.js
+++ src/sections/Page5.js
@@ -1,10 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './styles.css';
+import mainBgImage from './images-sections/bg-pic-main.jpg';
 
 export default class Page5 extends Component {
     render() {
+        let style = {
+            backgroundImage: `url(${mainBgImage})`
+        }
         return (
             <div>
-                
+                <div className="main_page_section" id="main">
+                <div style={style} className='main_section_background'>
+                    <div className="main_content_container">  
+                        <div className="main_headline">Section 5 Headline</div>
+                        <div className="main_paragraph_01">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
+            
+                    </div>   
+                </div>             
+            </div>
             </div>
         )
     }
