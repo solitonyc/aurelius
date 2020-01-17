783e6de686ee668d96c5b4028962c787c2bad787
Solito Reyes II <solitonyc@gmail.com>
2020-01-16 22:24:40 -0500

    app.js setup of routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import Navigation from './nav-bar/Navigation';
+import Main from './sections/Main';
+import Page2 from './sections/Page2';
+import Page3 from './sections/Page3';
+import Page4 from './sections/Page4';
+import Page5 from './sections/Page5';
+import Footer from './sections/Footer';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Main />
+        <Page2 />
+        <Page3 />
+        <Page4 />
+        <Page5 />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
