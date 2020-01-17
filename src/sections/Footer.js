3a2206a9cab3c68b04b26b5fe2d93675f6e130bf
Solito Reyes II <solitonyc@gmail.com>
2020-01-17 18:23:53 -0500

    added footer page w style

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './styles.css';
 
 export default class Footer extends Component {
     render() {
         return (
             <div>
+                <div className="main_page_section" id="main">
                 
+                    <div className="main_content_container">  
+                        <div className="main_headline">This is the footer section</div>
+                    
+            
+                    </div>   
+                            
+            </div>
             </div>
         )
     }
