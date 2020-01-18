3b10ffedb206903ed351090ad7fcc9b7b2e0e028
Solito Reyes II <solitonyc@gmail.com>
2020-01-18 10:20:48 -0500

    customized css classname for services page

--- src/sections/Services.js
+++ src/sections/Services.js
@@ -9,11 +9,11 @@ export default class Page2 extends Component {
         }
         return (
             <div>
-                <div className="main_page_section" id="services">
-                <div style={style} className='main_section_background'>
-                    <div className="main_content_container">  
-                        <div className="main_headline">Our Services</div>
-                        <div className="main_paragraph_01">
+                <div className="services_page_section" id="services">
+                <div style={style} className='services_section_background'>
+                    <div className="services_content_container">  
+                        <div className="services_headline">Our Services</div>
+                        <div className="services_paragraph_01">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
