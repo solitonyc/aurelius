7930e1e83500d70231725f59796774cab1683c4e
solitonyc <solito-air@beta-air.home>
2020-01-19 21:13:07 -0500

    footer content edit

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -12,7 +12,7 @@ export default class Footer extends Component {
                 <div className="back_to_top">
                     <Link onClick={this.scrollToTop} className="back_to_top">MAIN</Link>
                 </div>
-                <div className='footer_content'>Designed by XegoTech<br />Copyright &copy; 20120 Aurelius LLC<br />All Rights Reserved</div> 
+                <div className='footer_content'>Copyright &copy; 2020 Aurelius LLC<br />All Rights Reserved<br />Designed by XegoTech<br /></div> 
             </div>
         )
     }
