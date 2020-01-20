1a7352660a170a62199be60cd1d014ccfd9d8611
Solito Reyes II <solitonyc@gmail.com>
2020-01-20 17:10:16 -0500

    management page boxes added

--- src/sections/Management.js
+++ src/sections/Management.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './management.css';
 import mainBgImage from './images-sections/bg-img4.jpg';
+import philBoxImg01 from './images-sections/bg-img5.jpg';
+import philBoxImg02 from './images-sections/bg-img8.jpg';
 
 export default class Page4 extends Component {
     render() {
@@ -21,7 +23,28 @@ export default class Page4 extends Component {
                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </div>
-            
+                        <div className="mgmt_columns_container">
+                        <div className='mgmt_column_box'>
+                            <img className='mgmt_box_image' alt='' src={philBoxImg01} />
+                            <div className="mgmt_column_box_title">John Doe, CEO</div>                            
+                            <p className='mgmt_paragraph_box'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                                consequat. 
+                            </p>	                            
+                        </div>                    
+                        <div className="mgmt_column_box">
+                            <img className='mgmt_box_image' alt='' src={philBoxImg02} />
+                            <div className="mgmt_column_box_title">Jane Doe, COO</div>
+                            <p className='mgmt_paragraph_box'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                                consequat. 
+                            </p>
+                        </div>                    
+                    </div>                    
                     </div>   
                 </div>             
             </div>
